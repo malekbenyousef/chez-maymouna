ca3c2d3144e5dd6c27983f5ff58c02f77aa9011e
malekbenyousef <malkbenyoussef2002@gmail.com>
2026-05-30 16:24:45 +0300

    fixed database connection

--- db/pool.js
+++ db/pool.js
@@ -1,10 +1,4 @@
 const { Pool } = require("pg");
-const role = process.env.ROLE_NAME
-const passwrd = process.env.ROLE_PASSWORD
 module.exports = new Pool({
-  host: "localhost", 
-  user: role,
-  database: "chez_maymouna",
-  password: passwrd,
-  port: 5432 
+    connectionString: process.env.DATABASE_URL
 });
